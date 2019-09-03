ce2d3feee6d852093ff10b9c8d79c0973fb13fda
njtak2514 <njtak2514@gmail.com>
2019-09-03 18:19:00 +0900

    etc

--- vanillajs/WebContent/js/basic1_5.js
+++ vanillajs/WebContent/js/basic1_5.js
@@ -1,2 +1,35 @@
-alert('Im Working. Im JS. Im Beautiful. Im worth it');
-console.log('ur Working. ur JS. ur Beautiful. ur worth it');
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(251,197,49)";
+const OTHER_COLOR = "#fbc531";
+
+/*function handleResize(event){
+	console.log(event);
+	console.log("I have been resized");
+}*/
+
+function handleClick(){
+	const curruntColor = title.style.color;
+		if(curruntColor === BASE_COLOR){
+			title.style.color = OTHER_COLOR;
+		}else{
+			title.style.color = BASE_COLOR;
+		}
+}
+
+function init(){
+	title.style.color = BASE_COLOR;
+	title.addEventListener("click", handleClick);
+}
+
+init();
+
+function handleOffline(){
+	console.log("turn off the wifi");
+}
+function handleOnline(){
+	console.log("turn on the wifi");
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
