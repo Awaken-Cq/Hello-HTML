ec1531e94f666f8a80462a5701bc74cb39074233
njtak2514 <njtak2514@gmail.com>
2019-09-25 14:28:19 +0900

    2019.09.25 clone momentum

--- vanillajs/WebContent/js/basic1_5.js
+++ vanillajs/WebContent/js/basic1_5.js
@@ -1,35 +1,21 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(251,197,49)";
-const OTHER_COLOR = "#fbc531";
-
-/*function handleResize(event){
-	console.log(event);
-	console.log("I have been resized");
-}*/
+const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-	const curruntColor = title.style.color;
-		if(curruntColor === BASE_COLOR){
-			title.style.color = OTHER_COLOR;
-		}else{
-			title.style.color = BASE_COLOR;
-		}
+	const currentClass = title.className;
+	if(currentClass !== CLICKED_CLASS){
+		title.className = CLICKED_CLASS;
+	}else{
+		title.className = "";
+	}
+	
 }
+ 
 
 function init(){
-	title.style.color = BASE_COLOR;
+
 	title.addEventListener("click", handleClick);
 }
 
 init();
-
-function handleOffline(){
-	console.log("turn off the wifi");
-}
-function handleOnline(){
-	console.log("turn on the wifi");
-}
-
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
